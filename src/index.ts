6d8327078ef1c1d8f0951118f6ef6acef2dc25be
Murhaf Sousli <murhafsousli@gmail.com>
2017-10-29 21:06:18 +0300

    update

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { GalleryModule } from './gallery.module';
+export { Gallery } from './services/gallery.service';
+export {
+  GalleryConfig,
+  GalleryDescConfig,
+  GalleryThumbConfig,
+  GalleryNavConfig,
+  GalleryLoaderConfig,
+  GalleryPlayConfig,
+  GalleryBulletConfig,
+  GalleryState,
+  GalleryItem
+} from './models';
