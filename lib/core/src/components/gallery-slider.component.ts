d4c351b5068e27598074aedc1a6fd9468f6f899c
Murhaf Sousli <murhafsousli@gmail.com>
2018-03-12 01:55:07 +0300

    feat(GallerySlider): Rearrange slider on window resize, closes #67

--- lib/core/src/components/gallery-slider.component.ts
+++ lib/core/src/components/gallery-slider.component.ts
@@ -7,13 +7,20 @@ import {
   OnChanges,
   ChangeDetectionStrategy,
   ElementRef,
-  EventEmitter
+  EventEmitter,
+  Inject,
+  PLATFORM_ID
 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { GalleryState, GalleryConfig } from '../models';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 import { map } from 'rxjs/operators/map';
+import { tap } from 'rxjs/operators/tap';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { debounceTime } from 'rxjs/operators/debounceTime';
 
 declare const Hammer: any;
 
@@ -39,6 +46,7 @@ declare const Hammer: any;
 export class GallerySliderComponent implements OnInit, OnChanges, OnDestroy {
 
   mc: any;
+  resizeSub$: Subscription;
   sliderState$: Observable<any>;
   stateStream$ = new BehaviorSubject({value: 0, active: false});
   @Input() state: GalleryState;
@@ -47,7 +55,7 @@ export class GallerySliderComponent implements OnInit, OnChanges, OnDestroy {
   @Input() height: number;
   @Output() indexChange = new EventEmitter<string | number>();
 
-  constructor(private _el: ElementRef) {
+  constructor(private _el: ElementRef, @Inject(PLATFORM_ID) private platform: Object) {
     this.sliderState$ = this.stateStream$.pipe(map(
       (state: any) => ({
         style: this.sliderStyle(state.value),
@@ -107,6 +115,14 @@ export class GallerySliderComponent implements OnInit, OnChanges, OnDestroy {
       });
     }
 
+    // Rearrange slider on window resize
+    if (isPlatformBrowser(this.platform)) {
+      this.resizeSub$ = fromEvent(window, 'resize').pipe(
+        debounceTime(200),
+        tap(() => this.stateStream$.next(this.stateStream$.getValue()))
+      ).subscribe();
+    }
+
     // Fix wrong slider width on init
     setTimeout(() => {
       this.stateStream$.next({value: 0, active: false});
@@ -116,6 +132,9 @@ export class GallerySliderComponent implements OnInit, OnChanges, OnDestroy {
   ngOnDestroy() {
     if (this.mc) {
       this.mc.destroy();
+    }
+    if (this.resizeSub$) {
+      this.resizeSub$.unsubscribe();
     }
   }
 
