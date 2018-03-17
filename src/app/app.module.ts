e836bb558aeb6824a3e5beedac9fcb9a9ba5b2d9
Murhaf Sousli <murhafsousli@gmail.com>
2018-03-17 04:20:42 +0300

    Add universal to demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GalleryModule } from './gallery/core';
 import { LightboxModule } from './gallery/lightbox';
+import { GallerizeModule } from './gallery/gallerize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'my-app'}),
     BrowserAnimationsModule,
     GalleryModule.forRoot(),
-    LightboxModule.forRoot({
-      panelClass: 'fullscreen'
-    })
+    LightboxModule.forRoot(),
+    GallerizeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
