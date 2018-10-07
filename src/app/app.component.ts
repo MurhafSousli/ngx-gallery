283c7e96e3dd0225777cf02689961b83fcdc7bc4
Murhaf Sousli <murhafsousli@gmail.com>
2018-10-07 15:41:54 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { Gallery, GalleryRef, ImageItem, GalleryItem } from '@ngx-gallery/core';
-import { Lightbox } from '@ngx-gallery/lightbox';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,45 +6,5 @@ import { Lightbox } from '@ngx-gallery/lightbox';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
-
-  items: GalleryItem[];
-
-  imageData = [
-    {
-      srcUrl: 'assets/img1.jpg',
-      previewUrl: 'assets/img1.jpg'
-    },
-    {
-      srcUrl: 'assets/img2.jpg',
-      previewUrl: 'assets/img2.jpg',
-    },
-    {
-      srcUrl: 'assets/img3.jpg',
-      previewUrl: 'assets/img3.jpg',
-    },
-    {
-      srcUrl: 'assets/img4.jpg',
-      previewUrl: 'assets/img4.jpg',
-    }
-  ];
-
-  constructor(public gallery: Gallery, public lightbox: Lightbox) {
-  }
-
-  ngOnInit() {
-    // This is for Basic example
-    const galleryRef: GalleryRef = this.gallery.ref('basic-test');
-
-    this.items = this.imageData.map(item => {
-      return new ImageItem({ src: item.srcUrl, thumb: item.previewUrl});
-    });
-
-    // This is for Lightbox example
-    this.gallery.ref('lightbox').load(galleryRef.state.items);
-  }
-
-  openLightbox() {
-    this.lightbox.open(0, 'lightbox');
-  }
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing';
