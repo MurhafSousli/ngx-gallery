abb3135f07bc60ad97ff207d87cb478c486a5e54
Murhaf Sousli <murhafsousli@gmail.com>
2018-11-18 03:30:16 +0300

    refactor

--- projects/core/src/lib/components/templates/items.model.ts
+++ projects/core/src/lib/components/templates/items.model.ts
@@ -2,34 +2,37 @@ import { GalleryItem } from '../../models/gallery.model';
 import { GalleryItemType } from '../../models/constants';
 
 export class ImageItem implements GalleryItem {
-  readonly type = 'image'; //GalleryItemType.Image;
+  readonly type: string;
   readonly data: any;
 
   constructor(data: any) {
     this.data = data;
+    this.type = GalleryItemType.Image;
   }
 }
 
 export class VideoItem implements GalleryItem {
-  readonly type = 'video'; // GalleryItemType.Video;
+  readonly type: string;
   readonly data: any;
 
   constructor(data: any) {
     this.data = data;
+    this.type = GalleryItemType.Video;
   }
 }
 
 export class IframeItem implements GalleryItem {
-  readonly type = 'iframe'; // GalleryItemType.Iframe;
+  readonly type: string;
   readonly data: any;
 
   constructor(data: any) {
     this.data = data;
+    this.type = GalleryItemType.Iframe;
   }
 }
 
 export class YoutubeItem implements GalleryItem {
-  readonly type = 'youtube'; // GalleryItemType.Youtube;
+  readonly type: string;
   readonly data: any;
 
   constructor(data: any) {
@@ -37,5 +40,6 @@ export class YoutubeItem implements GalleryItem {
       src: `//youtube.com/embed/${data.src}?wmode=transparent`,
       thumb: data.thumb ? data.thumb : `//img.youtube.com/vi/${data.src}/default.jpg`
     };
+    this.type = GalleryItemType.Youtube;
   }
 }
