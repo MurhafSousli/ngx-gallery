65e0e1d3d0942f28ababe2c492a635442c8e13c6
Murhaf Sousli <murhafsousli@gmail.com>
2021-12-15 18:19:50 +0100

    Fix package exports

--- projects/ng-gallery/lightbox/src/public_api.ts
+++ projects/ng-gallery/lightbox/src/public_api.ts
@@ -2,4 +2,5 @@ export * from './lightbox.model';
 export * from './lightbox.component';
 export * from './lightbox.service';
 export * from './gallerize.directive';
+export * from './lightbox.directive';
 export * from './lightbox.module';
