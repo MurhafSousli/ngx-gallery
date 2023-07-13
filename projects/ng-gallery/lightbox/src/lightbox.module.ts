9eecfdb2c1ec950b00d0714dcaf11b14cc32cffe
Murhaf Sousli <murhafsousli@gmail.com>
2023-07-13 09:10:27 +0300

    export GalleryModule from the LightboxModule

--- projects/ng-gallery/lightbox/src/lightbox.module.ts
+++ projects/ng-gallery/lightbox/src/lightbox.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { GalleryModule } from 'ng-gallery';
 import { LightboxDirective } from './lightbox.directive';
 import { GallerizeDirective } from './gallerize.directive';
 
 @NgModule({
-  imports: [LightboxDirective, GallerizeDirective  ],
-  exports: [LightboxDirective, GallerizeDirective  ]
+  imports: [GalleryModule, LightboxDirective, GallerizeDirective],
+  exports: [GalleryModule, LightboxDirective, GallerizeDirective]
 })
 export class LightboxModule {
 }
