2e42047fd58596eb2625a5b214903de106d85f4a
Murhaf Sousli <murhafsousli@gmail.com>
2024-08-27 05:43:21 +0200

    Update demo

--- projects/ng-gallery-demo/src/app/app.config.ts
+++ projects/ng-gallery-demo/src/app/app.config.ts
@@ -1,42 +1,41 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { NgProgressModule } from 'ngx-progressbar';
-import { NgProgressHttpModule } from 'ngx-progressbar/http';
-import { NgProgressRouterModule } from 'ngx-progressbar/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
-import { NG_SCROLLBAR_OPTIONS } from 'ngx-scrollbar';
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { AppRoutingModule } from './routing.module';
+import { provideScrollbarOptions } from 'ngx-scrollbar';
+import { provideHighlightOptions } from 'ngx-highlightjs';
+import { progressInterceptor } from 'ngx-progressbar/http';
+import { appRoutes } from './routing.module';
+import { provideNgProgressRouter } from 'ngx-progressbar/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(BrowserModule, AppRoutingModule, NgProgressModule, NgProgressHttpModule, NgProgressRouterModule),
     {
       provide: GALLERY_CONFIG,
       useValue: {
         imageSize: 'cover'
       } as GalleryConfig
     },
-    {
-      provide: HIGHLIGHT_OPTIONS,
-      useValue: {
-        coreLibraryLoader: () => import('highlight.js/lib/core'),
-        languages: {
-          typescript: () => import('highlight.js/lib/languages/typescript'),
-          css: () => import('highlight.js/lib/languages/css'),
-          xml: () => import('highlight.js/lib/languages/xml')
-        }
+    provideRouter(appRoutes, withHashLocation()),
+    provideHighlightOptions({
+      coreLibraryLoader: () => import('highlight.js/lib/core'),
+      languages: {
+        typescript: () => import('highlight.js/lib/languages/typescript'),
+        css: () => import('highlight.js/lib/languages/css'),
+        xml: () => import('highlight.js/lib/languages/xml')
       }
-    },
-    {
-      provide: NG_SCROLLBAR_OPTIONS,
-      useValue: {
-        appearance: 'compact'
-      }
-    },
+    }),
+    provideScrollbarOptions({
+      appearance: 'compact'
+    }),
     provideAnimations(),
-    provideHttpClient(withInterceptorsFromDi())
+    provideNgProgressRouter({
+      minDuration: 600
+    }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([progressInterceptor])
+    )
   ]
 };
